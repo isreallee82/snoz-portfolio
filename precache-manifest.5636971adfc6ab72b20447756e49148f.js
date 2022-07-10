d5d6ac410802f2d4663f8a060fba0a97b4864756
isreallee <isreallee82@gmail.com>
2022-07-10 13:54:55 +0100

    Updates

--- precache-manifest.5636971adfc6ab72b20447756e49148f.js
+++ precache-manifest.5636971adfc6ab72b20447756e49148f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f08704d90281c460a4a07799aa55417d",
+    "revision": "af1178576cc6d37bdc0926b9ff414a4a",
     "url": "/snoz-portfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/snoz-portfolio/static/css/2.d39e68e1.chunk.css"
   },
   {
-    "revision": "506b422ff22afa694822",
-    "url": "/snoz-portfolio/static/css/main.a26a1d03.chunk.css"
+    "revision": "9de68ca96babb375b93b",
+    "url": "/snoz-portfolio/static/css/main.1ccd5b2b.chunk.css"
   },
   {
     "revision": "426ca811ebe76450fb7d",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/snoz-portfolio/static/js/2.285bc02f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "506b422ff22afa694822",
+    "revision": "9de68ca96babb375b93b",
     "url": "/snoz-portfolio/static/js/main.8ebbdfd0.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/snoz-portfolio/precache-manifest.e4d479795909ae5a9d51a263a68171dc.js"
+  "/snoz-portfolio/precache-manifest.5636971adfc6ab72b20447756e49148f.js"
 );
 
 self.addEventListener('message', (event) => {
