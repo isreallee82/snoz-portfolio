b95663d28ca34ccc0366c9fd29e651f1f304e6cb
isreallee <isreallee82@gmail.com>
2022-07-10 13:47:49 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/snoz-portfolio/precache-manifest.5b3b725172e428ffdca79c69a1869759.js"
+  "/snoz-portfolio/precache-manifest.3a7ace441135de2dd0c39582c4b2f61e.js"
 );
 
 self.addEventListener('message', (event) => {
